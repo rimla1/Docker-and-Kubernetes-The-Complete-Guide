b13ac55f0cd8505321c29aaa5a9559ff2d86b536
Almir Muminovic <projectalmir@gmail.com>
2022-09-19 14:50:44 +0200

    Copying first json file and after that all other files

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
-const app = express()
+const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hi there!")
-})
+  res.send("Hi there with additional change!");
+});
 
-app.listen(8080, () => {
-    console.log("Listening on port 8080")
-})
+app.listen(9000, () => {
+  console.log("Listening on port 9000");
+});
