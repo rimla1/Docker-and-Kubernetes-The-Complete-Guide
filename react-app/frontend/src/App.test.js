0d43102f0e791a73847a6ed98d072b908951934d
Almir Muminovic <projectalmir@gmail.com>
2022-10-24 13:52:59 +0200

    Solving live testing using docker-compose with creating another container

--- react-app/frontend/src/App.test.js
+++ react-app/frontend/src/App.test.js
@@ -7,7 +7,19 @@ test("renders learn react link", () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test("renders learn react link", () => {
+test("renders learn react link 2", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link 3", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link 4", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
